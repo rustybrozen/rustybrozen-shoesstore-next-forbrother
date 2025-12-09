143cb9fced6d15968045611081f3ae9356dfc4db
Russel <zachudson12@gmail.com>
2025-12-09 10:18:59 +0700

    first commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,18 @@
 // lib/db.ts
 import Database from 'better-sqlite3';
+import path from 'path';
+import fs from 'fs';
 
-const db = new Database('shop.db'); 
+// Tạo đường dẫn vào folder data
+const dbPath = path.join(process.cwd(), 'data', 'shop.db');
+
+// Đảm bảo folder data tồn tại (phòng hờ)
+const dir = path.dirname(dbPath);
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+}
+
+const db = new Database(dbPath);
 
 
 db.pragma('journal_mode = WAL');
